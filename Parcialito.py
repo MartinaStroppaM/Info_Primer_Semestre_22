11c0cd1ac1dd98bc70a1ffbdde539a91e9336202
MartinaStroppaM <martism.2014@gmail.com>
2022-05-02 11:28:05 -0300

    actualización

---
+++ Parcialito.py
@@ -0,0 +1,16 @@
+#1
+
+import re
+
+def cuantas_veces(string):
+    resultado = re.findall)
+
+def sin_C(string):
+    return re.findall("aa([^c]*?)gg", string)
+
+#3
+
+"""
+¿Qué otras excepciones deberías considerar?
+Se podría verificar que sea una lista, ya que si el parámetro es de otro tipo (no iterable) el for no va a funcionar
+"""
